157cbede7a1dfbb9fc279779aaee9a10d3d58224
Robert Fischer <lilbfisch@gmail.com>
2017-08-28 20:10:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,3 +27,7 @@ function removeLastKitten() {
   var kittensArr = kittens.slice(0, kittens.length - 1)
   return kittensArr
 }
+function removeFirstKitten() {
+  var kittensArr = kittens.slice(1)
+  return kittensArr
+}
